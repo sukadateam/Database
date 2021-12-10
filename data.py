9c255b9ff907eb0bf8a274cc15c57f3f957cf3ff
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-09 19:45:13 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 data_bases=[['basic', True, 'all', 'column_row', ['name','id','gender']],['chance', True, 'all', 'list']]
 #Name, Active status, owner, type, columns
-row=[['basic',['Brandon','123456','Male']],['basic', ['Gertie','124551','Male']]]
+row=[['basic',['Brandon','123456','Male']],['basic', ['Gertie','124551','Male']], ['basic', ['Fluffy','124411','Trans']]]
 #Side to side
 #Data_base, items in row
-lists=[['basic', ['money','today']]]
+lists=[['chance', ['money','today']]]
 #Lists
 #Data_base, items in list
 
@@ -16,20 +16,6 @@ permissions=['admin']
 global_password=None #A password that can be used on anything. If none, then no global_password is allowed.
 allowed_types=['column_row', 'list', 'grid']
 allowed_users=['all', 'admin']
+min_length=5 #Min password length
+max_length=8 #Max password length
 debug=True
-
-
-
-
-
-
-
-#Reset ----(DO NOT TOUCH)----
-data_bases_reset=[['basic', True, 'all', 'column_row', ['name','id','gender']],['chance', True, 'all', 'list']]
-row_reset=[['basic',['Brandon','123456','Male']],['basic', ['Gertie','124551','Male']]]
-lists_reset=[['basic', ['money','today']]]
-allowed_types_reset=['column_row', 'list', 'grid']
-allowed_users_reset=['all', 'admin']
-known_users_reset=['Admin']
-passwords_reset=[None]
-permissions_reset=['admin']
