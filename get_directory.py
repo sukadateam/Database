0d6b638b8b763ac2d71cbe49d04ed8c4761262b8
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-10 21:36:16 -0500

    Delete get_directory.py

--- get_directory.py
+++
@@ -1,7 +0,0 @@
-import os
-path=os.path.dirname(os.path.realpath(__file__))
-path=path.replace('\\','\\\\')
-os.chdir(path)
-file=open('directory.py','w')
-file.write('path="'+str(path)+'"')
-file.close()
