39f73f615ebad6f2a8a426f5dd8a824c8034aba4
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-10 21:35:56 -0500

    Delete cache.py

--- cache.py
+++
@@ -1,2 +0,0 @@
-#File will be used later on in later versions if system this program will be running of has very little ram.
-#This will make the program run slower, but may require less ram in the long run.
