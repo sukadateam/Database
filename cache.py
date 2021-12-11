d0cdde52112ab1bae6ab857f1efa40384a401666
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-10 20:08:42 -0500

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,3 +1,2 @@
-sets=None
-rows=None
-type=None
+#File will be used later on in later versions if system this program will be running of has very little ram.
+#This will make the program run slower, but may require less ram in the long run.
