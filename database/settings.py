ececc56ed4133df95da11efbd30389ed90c9d47c
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-12 18:04:00 -0500

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -1,3 +1,7 @@
-do_not_remove=True
+#If True system will not remove files after encrypt and decrypt. Will remove files if set to False.
+do_not_remove=False
+fail_safe=True
+#Required python version to run program.
 required_version='3.10.1'
+#Application version. Just for show.
 program_version='0.1.6'
