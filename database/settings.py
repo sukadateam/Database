90137668a66a220aebeaea26ad2c568be89848a4
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-13 07:55:46 -0500

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -4,4 +4,8 @@ fail_safe=True
 #Required python version to run program.
 required_version='3.10.1'
 #Application version. Just for show.
-program_version='0.1.6'
+program_version='0.1.7'
+#Drive letter to store hash.aes file on root directory. Letter must be Uppercase. Windows only.
+drive_letter='E'
+#Operating System or OS. Can be windows or linux. Must be lowercase.
+os='windows'
