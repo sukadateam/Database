e86e2d9a4af3cb629f555e69e50a924f0f0bb5c6
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-13 17:45:36 -0500

    Update files_to_backup.py

--- database/files_to_backup.py
+++ database/files_to_backup.py
@@ -1,1 +1,1 @@
-backup_files=['data.py','directory.py','exit.py','files_to_backup.py','get_directory.py','reset.py','settings.py','shell.py','vars_to_save.py']
+backup_files=['data.py','directory.py','files_to_backup.py','get_directory.py','reset.py','settings.py','shell.py','vars_to_save.py']
