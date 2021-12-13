a5526974ea703755b275dca6972c88b910bfae4e
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-13 13:19:28 -0500

    Update shell.py

--- database/shell.py
+++ database/shell.py
@@ -1,8 +1,10 @@
 from custom_database import *
-choice = input('(1)Create new hash\n(2)Encrypt database\n(3)Decrypt database\nYour choice: ')
+choice = input('(1)Create new hash\n(2)Encrypt database\n(3)Decrypt database\n(4)Backup\nYour choice: ')
 if choice == "1":
     get.new_hash() #Makes a new hash
 if choice == "2":
     encrypt.all(password=str(input('Password: ')))
 if choice == "3":
     decrypt.all(password=str(input('Password: ')))
+if choice == "4":
+    backup.all()
