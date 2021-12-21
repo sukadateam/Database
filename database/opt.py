b787c86b63e66290fdf072a8a84c8200104ca865
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-21 09:13:00 -0500

    Delete opt.py

--- database/opt.py
+++
@@ -1,9 +0,0 @@
-#Add later
-def show_databases():
-    data_base.show.all_info()
-def show_database(var):
-    data_base.show.all(var)
-def show_lists(data_base=None):
-    data_base.show.show_lists(data_base=data_base)
-def show_row(data_base=None):
-    data_base.show.show_row(data_base=data_base)
