fc401bc19c0688d529f948ea8528f5308feba368
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-22 18:14:16 -0500

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -5,18 +5,20 @@ fail_safe=True
 #Required python version to run program.
 required_version='3.10.1'
 #Application version. Just for show.
-program_version='0.2.3'
+program_version='0.2.4'
 #Drive letter to store hash.aes file on root directory. Letter must be Uppercase. Windows only.
 drive_letter='E'
 #Drive name to store hash.aes file on root directory. Linux only.
 drive_name='Computer'
 #Operating System or OS. Can be windows or linux. Must be lowercase.
 system='windows'
-#Filters bad words that people should not be using. USE AT YOUR OWN RISK. Currently in development phaze.
+#Filters bad words that people should not be using. USE AT YOUR OWN RISK. Currently in development phase.
 profanity_filter=False
-#Disable profanity filter for admin. USE AT YOUR OWN RISK. Currently in development phaze.
+#Disable profanity filter for admin. USE AT YOUR OWN RISK. Currently in development phase.
 disable_filter_admin=False
 #A backup password in case the other is forgotten.
 global_password=True
-#Don't load save file. True: Skip save file -- False: Load defualt file not save file. USE AT YOUR OWN RISK. Currently in development phaze.
+#Don't load save file. True: Skip save file -- False: Load defualt file not save file. USE AT YOUR OWN RISK. Currently in development phase.
 dont_load_save=False
+#Automatically optimize on start up. USE AT YOUR OWN RISK. Currently in development phase.
+optimize_on_startup=False
