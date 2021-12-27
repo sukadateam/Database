f5c4cf7c47ea4e19c3d7e5a3f62eaded265cbd89
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-26 20:38:22 -0500

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -1,3 +1,5 @@
+#Do not make any vars equal numbers. True or False are fine and "" are fine.
+
 #If True system will not remove files after encrypt and decrypt. Will remove files if set to False.
 do_not_remove=False
 #Do not disable failsafe unless needed! Trust me. Don't disable it.
@@ -10,15 +12,15 @@ program_version='0.2.5'
 drive_letter='E'
 #Drive name to store hash.aes file on root directory. Linux only.
 drive_name='Computer'
-#Operating System or OS. Can be windows or linux. Must be lowercase.
+#Operating System or OS. Can be macos, windows or linux. Must be lowercase.
 system='windows'
 #Filters bad words that people should not be using. USE AT YOUR OWN RISK. Currently in development phase.
 profanity_filter=False
-#Disable profanity filter for admin. USE AT YOUR OWN RISK. Currently in development phase.
+#Disable profanity filter for admin.
 disable_filter_admin=False
 #A backup password in case the other is forgotten.
 global_password=True
-#Don't load save file. True: Skip save file -- False: Load defualt file not save file. USE AT YOUR OWN RISK. Currently in development phase.
+#Don't load save file. True: Skip save file -- False: Load defualt file not save file.
 dont_load_save=False
 #Automatically optimize on start up. USE AT YOUR OWN RISK. Currently in development phase.
 optimize_on_startup=False
@@ -26,3 +28,14 @@ optimize_on_startup=False
 auto_history_record=True
 #App version control --(Disable if needed)--
 app_version_control=True
+#Only allow set operating system. Change system variable to your choice. USE AT YOUR OWN RISK. Currently in development phase.
+set_operating_system=True
+#Allowed windows versions. You can choose 7, 8, 10, 11. Only works if system setting is set to windows.
+allow_windows_version='10'
+
+
+
+
+#Settings coming soon
+#If a record prior what's asked is a duplicate, system will then ignore the task given.
+skip_history_copy=False
