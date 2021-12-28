ae94300e1473f3cc4c8de7ece07aff2279be336e
sukadateam <34036185+sukadateam@users.noreply.github.com>
2021-12-28 12:01:46 -0500

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -15,7 +15,7 @@ drive_name='Computer'
 #Operating System or OS. Can be macos, windows or linux. Must be lowercase.
 system='windows'
 #Filters bad words that people should not be using. USE AT YOUR OWN RISK. Currently in development phase.
-profanity_filter=False
+profanity_filter=True
 #Disable profanity filter for admin.
 disable_filter_admin=False
 #A backup password in case the other is forgotten.
@@ -28,7 +28,7 @@ optimize_on_startup=False
 auto_history_record=False
 #App version control --(Disable if needed)--
 app_version_control=True
-#Only allow set operating system. Change system variable to your choice. USE AT YOUR OWN RISK. Currently in development phase.
+#Only allow set operating system. Change system variable to your choice.
 set_operating_system=True
 #Allowed windows versions. You can choose 7, 8, 10, 11. Only works if system setting is set to windows.
 allow_windows_version='10'
@@ -36,12 +36,18 @@ allow_windows_version='10'
 
 
 
-#Settings coming soon
+#Settings coming soon. Do not change unless your a dare devil.
 #If a record prior what's asked is a duplicate, system will then ignore the task given.
 skip_history_copy=False
-#Enable automated history_file for functions
+#Enable automated history_file for functions. 
 auto_error_record=True
 #Assign Digit number to history item for a more depth look into the item. And create a database to handle all the data for each assigned item.
 assign_digit_forHistory=False
 #How many digits are allowed to be used to store history.
 allowed_digists_forHistory=8
+#An attempt to use more than one cpu core.
+multi_process=True
+#Automatically check inputs for profanity. May use more CPU power than normal.
+auto_filter_profanity=False
+#Attempt to speed up the search on inputs by using a smaller version of profanity.txt. Only works if auto_filter_profanity is set to True.
+auto_filter_profanity_speedBoost=False
