f41a65463cd20dae89a73f4c860c66785407664d
sukadateam <34036185+sukadateam@users.noreply.github.com>
2022-01-12 09:55:31 -0500

    Update settings.py
    Optimized settings

--- database/settings.py
+++ database/settings.py
@@ -7,7 +7,7 @@ fail_safe=True
 #Required python version to run program.
 required_version='3.10.1'
 #Application version. Just for show.
-program_version='0.2.9'
+program_version='0.3.0'
 #Drive letter to store hash.aes file on root directory. Letter must be Uppercase. Windows only.
 drive_letter='E'
 #Drive name to store hash.aes file on root directory. Linux only.
@@ -21,15 +21,15 @@ disable_filter_admin=False
 #A backup password in case the other is forgotten.
 global_password=True
 #Don't load save file. True: Skip save file -- False: Load defualt file not save file.
-dont_load_save=True
+dont_load_save=False
 #Automatically optimize on start up. USE AT YOUR OWN RISK. Currently in development phase.
 optimize_on_startup=False
 #Enable automated history_file for functions
 auto_history_record=True
 #App version control --(Disable if needed)--
-app_version_control=True
+app_version_control=False
 #Only allow set operating system. Change system variable to your choice.
-set_operating_system=True
+set_operating_system=False
 #Allowed windows versions. You can choose 7, 8, 10, 11. Only works if system setting is set to windows.
 allow_windows_version='10'
 #If a record prior what's asked is a duplicate, system will then ignore the task given.
@@ -41,19 +41,19 @@ quit_ifIncorrect=True
 #Record a more informed description of a item in history.txt
 advanced_history=True
 #Automatically check inputs for profanity. May use more CPU power than normal.
-auto_filter_profanity=False
+auto_filter_profanity=True
 #Attempt to speed up the search on inputs by using a smaller version of profanity.txt. Only works if auto_filter_profanity is set to True.
 auto_filter_profanity_speedBoost=False
 #An attempt to use more than one cpu core.
 multi_process=False
 #Assign Digit number to history item for a more depth look into the item. And create a database to handle all the data for each assigned item.
-assign_digit_forHistory=False
+assign_digit_forHistory=True
 #How many digits are allowed to be used to store history. Max 30.
 allowed_digists_forHistory=8
 #Will still check for incorrect settings if quit_ifIncorrect is True, but won't display anything.
 show_incorrect_settings=True
 #Will deny the program from saving. No matter what. Will cause problems for long term.
-disable_save=True
+disable_save=False
 #min and max password lengths, and allowed characters
 min_length=5
 max_length=8
@@ -76,3 +76,5 @@ text_color='#494b4d'
 button_height=2
 button_width=15
 text_font=30
+
+
