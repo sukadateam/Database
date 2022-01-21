8d0fda3770e3c7ae292c8a9f2b603e4988c52bb1
sukadateam <34036185+sukadateam@users.noreply.github.com>
2022-01-21 11:31:02 -0500

    Update vars_to_save.py

--- database/vars_to_save.py
+++ database/vars_to_save.py
@@ -1,1 +1,1 @@
-list=['active_users','denied_inputs','data_bases','row','lists','known_users','passwords','permissions','global_password','allowed_types','allowed_users','min_length','max_length','debug','opto_data','opto_row','opto_lists']
+list=['students','active_users','denied_inputs','data_bases','row','lists','known_users','passwords','permissions','global_password','allowed_types','allowed_users','min_length','max_length','debug','opto_data','opto_row','opto_lists']
