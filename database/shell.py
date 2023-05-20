cf6a07fb780a190606799be1e9debd199f4e7970
sukadateam <34036185+sukadateam@users.noreply.github.com>
2023-05-19 21:41:19 -0400

    Update shell.py

--- database/shell.py
+++ database/shell.py
@@ -1,3 +1,4 @@
+0.9.1
 #add_column has had no testing done at all.
 #remove_column has had no testing done at all.
 #Optimize save file has had no test done at all.
@@ -6,73 +7,97 @@ profanityFilter.setup()
 while True:
     for i in range(10):
         print('')
-    if users.return_login_cred() == False:
-        choice = input('(1)Create new hash\n(2)Encrypt database\n(3)Decrypt database\n(4)Backup\n(5)Create database\n(6)Save\n(7)Edit database\n(8)Optimize save file\n(9)Check profanity\n(10)Decrypt file\n(11)Encrypt file\n(12)Login\nYour choice: ')
+    var1, var2=users.return_login_cred()
+    if var1 == 'UserNotSignedIn':
+        choice = input('(1)Create new hash\n(2)Encrypt database\n(3)Decrypt database\n(4)Check Encryption password\n(5)Backup\n(6)Create database\n(7)Save\n(8)Edit database\n(9)Optimize save file\n(10)Check profanity\n(11)Decrypt file\n(12)Encrypt file\n(13)Login\nYour choice: ')
     else:
-        choice = input('(1)Create new hash\n(2)Encrypt database\n(3)Decrypt database\n(4)Backup\n(5)Create database\n(6)Save\n(7)Edit database\n(8)Optimize save file\n(9)Check profanity\n(10)Decrypt file\n(11)Encrypt file\n(12)Logout\nYour choice: ')
+        choice = input('(1)Create new hash\n(2)Encrypt database\n(3)Decrypt database\n(4)Check Encryption password\n(5)Backup\n(6)Create database\n(7)Save\n(8)Edit database\n(9)Optimize save file\n(10)Check profanity\n(11)Decrypt file\n(12)Encrypt file\n(13)Logout\nYour choice: ')
     os.system("cls" if os.name == "nt" else "clear")
     if choice == "exit":
         os.system("cls" if os.name == "nt" else "clear")
         safe_exit.close(create_backup=True, encryption_passw=input('Encryption Password: '), hide=True, random_name=True)
-    if choice=='11':
+    if choice=='12':
         try:
             file=input('What file: ')
             pyAesCrypt.encryptFile(file+'.py', file+'.aes', passw=input('Password: '))
             os.system("cls" if os.name == "nt" else "clear")
         except:
             print('File does not exist.')
-    if choice=='10':
+    if choice=='11':
         try:
             file=input('What file: ')
             pyAesCrypt.decryptFile(file+'.aes', file+'.py', passw=input('Password: '))
         except:
             print('Incorrect password or file does not exist.')
-    if choice == "9":
+    if choice == "10":
         print(list1)
         if profanityFilter.filter(var=input('Your input: ').lower()) == 1:
             print('Match Found')
         else:
             print('Match Not Found')
-    if users.return_login_cred() != False:
-        if choice=="12":
+    if var1 != 'UserNotSignedIn':
+        if choice=="13":
             users.logout()
             choice=''
-    if users.return_login_cred() == False:
-        if choice=="12":
+    if var1 == 'UserNotSignedIn':
+        if choice=="13":
             users.login_request(user=input('Username: '), password=input('Password: '))
     if choice == "1":
-        get.new_hash() #Makes a new hash
+        print('Enter the new passwords below: ')
+        get.new_hash(other=False, passw=input('Primary Password: ')) #Makes a new hash
         get.encrypt_hash(other=True, passw=input('Backup Password: '))
     if choice == "2":
-        encrypt.all(password=str(input('Password: ')))
+        try:
+            encrypt.all(password=str(input('Password: ')))
+        except:
+            print('Incorrect Password or Error. Calling function encrypt.all() from shell choice=2')
+        try:
+            os.remove('data_save.py')
+        except:
+            pass
     if choice == "3":
         decrypt.all(password=str(input('Password: ')))
     if choice == "4":
-        backup.create(password=input('Enter Your Encryption Password: '))
+        if check.encyption_password(input('Enter Your Encryption Password: ')) == 1:
+            print('Password is not a match! :(')
+        else:
+            print("Password is a match! :)")
     if choice == "5":
-        data_base.create.database(data_base=str(input('Enter the new databases name: ')).lower(), type=str(input('column_row or list: ')))
+        backup.create(password=input('Enter Your Encryption Password: '))
     if choice == "6":
+        data_base.create.database(data_base=str(input('Enter the new databases name: ')).lower(), type=str(input('column_row or list: ')))
+    if choice == "7":
         save.all()
-    if choice == "8":
+    if choice == "9":
         choice=input('Would you like to save (y/n): ').lower()
         if choice == "y":
             optimize.run(save_optimizations=True)
         if choice == "n":
             optimize.run(save_optimizations=False)
         choice = ''
-    if choice == "7":
+    if choice == "8":
         choice=input('\n\n\n(1)Users\n(2)Add row\n(3)Show databases\n(4)Delete database\n(5)Remove row\n(6)Show items in database\n(7)Add Column\n(8)Remove Column\n(9)Overload\nYour choice: ')
         if choice == "9":
             import random
-            dat=input('Enter database to overload: ')
-            for i in range(int(input('Choose a Number(100-10000);No comas: '))):
-                a=''
-                b=''
-                for i in range(8):
-                    a+=random.choice('12q9fhecycgy43gf6ucw4b6fxnc')
-                for i in range(8):
-                    b+=random.choice('12q9fhecycgy43gf6ucw4b6fxnc')
-                data_base.edit.add_row(data_base=dat, new_row=[a, b], split=False)
+            os.system('clear')
+            while True:
+                dat=input('Type /list for databases or /exit or \nEnter database to overload: ')
+                os.system('clear')
+                if dat == "/list":
+                    data_base.show.all_data_bases()
+                elif dat == "/exit":
+                    break
+                else:
+                    break
+            if dat != '/exit' and dat != "/list":
+                for i in range(int(input('Choose a Number(100-10000);No comas: '))):
+                    a=''
+                    b=''
+                    for i in range(8):
+                        a+=random.choice('12q9fhecycgy43gf6ucw4b6fxnc')
+                    for i in range(8):
+                        b+=random.choice('12q9fhecycgy43gf6ucw4b6fxnc')
+                    data_base.edit.add_row(data_base=dat, new_row=[a, b], split=False)
             save.all()
         if choice == "1":
             other=input('\n\n\n(1)Create user\n(2)Remove user\nYour choice: ')
